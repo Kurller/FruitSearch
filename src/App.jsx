8ce055675e11a25c5aa93dce16df696df1542b82
Kurller <kolaquadry@gmail.com>
2025-11-16 15:46:01 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { FruitsSearch } from "./fruitSearch";
+
+function App() {
+  return (
+    <div>
+      <h1>Fruit Search Application</h1>
+      <FruitsSearch />
+    </div>
+  );
+}
+
+export default App;
